39ebb366405a73e2bb071a0d60eda7c71111117b
sam blenny <68084116+samblenny@users.noreply.github.com>
2026-03-26 02:06:13 +0000

    placeholder app_main with tood

--- main/nanoc6-lua.c
+++ main/nanoc6-lua.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
 
 void app_main(void)
 {
-
+    for(;;) {
+        printf("TODO: Lua interpreter here\n");
+        vTaskDelay(pdMS_TO_TICKS(5000)); // sleep 5 seconds
+    }
 }

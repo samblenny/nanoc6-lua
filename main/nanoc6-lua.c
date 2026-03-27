c228b776460e5daab1558bb2e82cc5d69c74258a
sam blenny <68084116+samblenny@users.noreply.github.com>
2026-03-27 00:44:08 +0000

    try out esp_console repl demo
    This calls the REPL demo helper function thing used by many of the
    esp-idf examples. The REPL has a bunch of built in assumptions that
    aren't helpful. But, it does demonstrate a working readline
    capability using linenoise. Backspace editing works. Arrow key
    escape sequence can work (though it's turned off at the moment).
    
    The readline functionality for this demo is a big improvement over
    the low level API stuff that I was attempting to use earlier. It
    works like a readline is supposed to work, and it doesn't crash!

--- main/nanoc6-lua.c
+++ main/nanoc6-lua.c
@@ -1,35 +1,16 @@
 #include <stdio.h>
+#include "esp_system.h"  /* ESP_ERR_CHECK */
+#include "esp_console.h"
+#include "linenoise/linenoise.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
-#include "driver/usb_serial_jtag.h"
-#include "driver/usb_serial_jtag_vfs.h"
 #include "lua.h"
 #include "lauxlib.h"
 #include "lualib.h"
-#include <unistd.h>  /* for fsync */
+#include <unistd.h>  /* fsync */
 #include <fcntl.h>
 
-
-static int usb_readline(char *buf, int maxlen) {
-    int i = 0;
-    while (i < maxlen - 1) {
-        int c = fgetc(stdin);
-        if (c == EOF) {
-            vTaskDelay(pdMS_TO_TICKS(10)); // spurious EOF, yield and retry
-            continue;
-        }
-        fputc(c, stdout); // echo
-        fflush(stdout);
-        fsync(fileno(stdout));  // force USB packet
-        if (c == '\n' || c == '\r') {
-            break;
-        }
-        buf[i++] = (char)c;
-    }
-    buf[i] = '\0';
-    return i;
-}
-
+/*
 // Lua gets its own task so it can have a bigger stack than app_main default
 static void lua_task(void *pvParameters) {
     char buff[256];
@@ -57,31 +38,31 @@ static void lua_task(void *pvParameters) {
     lua_close(L);
     vTaskDelete(NULL);
 }
+*/
 
 void app_main(void) {
-    // Try to make USB serial input work better
-    // see esp-idf/examples/system/console/advanced/main/console_settings.c
-    usb_serial_jtag_vfs_set_rx_line_endings(ESP_LINE_ENDINGS_CR);
-    usb_serial_jtag_vfs_set_tx_line_endings(ESP_LINE_ENDINGS_CRLF);
+    esp_console_repl_t *repl = NULL;
+    esp_console_repl_config_t repl_config = ESP_CONSOLE_REPL_CONFIG_DEFAULT();
+    repl_config.prompt = ">";
+    repl_config.max_cmdline_length = 128;
 
-    // Blocking mode
-    fcntl(fileno(stdout), F_SETFL, 0);
-    fcntl(fileno(stdin), F_SETFL, 0);
+    esp_console_dev_usb_serial_jtag_config_t hw_config = 
+        ESP_CONSOLE_DEV_USB_SERIAL_JTAG_CONFIG_DEFAULT();
+    ESP_ERROR_CHECK(
+        esp_console_new_repl_usb_serial_jtag(&hw_config, &repl_config, &repl)
+    );
 
-    // Begin using interrupt-driven driver
-    usb_serial_jtag_driver_config_t cfg = {
-        .rx_buffer_size = 256,
-        .tx_buffer_size = 256,
-    };
-    usb_serial_jtag_driver_install(&cfg);
-    usb_serial_jtag_vfs_use_driver();
+    // This makes linenoise act as a vanilla dumb terminal readline without
+    // any of its default weirdness. Without this, the cursor flickers between
+    // the edit point and the first character of the line.
+    linenoiseSetDumbMode(1);
 
-    // Disable stream buffering
-    setvbuf(stdin, NULL, _IONBF, 0);
-    setvbuf(stdout, NULL, _IONBF, 0);
+    // This is a REPL helper function used by many esp-idf examples
+    ESP_ERROR_CHECK(esp_console_start_repl(repl));
+
 
     // start interpreter with 64KB stack
-    xTaskCreate(lua_task, "lua", 65536, NULL, 5, NULL);
+//    xTaskCreate(lua_task, "lua", 65536, NULL, 5, NULL);
     // make sure app_main exiting doesn't cause problems
-    vTaskDelete(NULL);
+//    vTaskDelete(NULL);
 }

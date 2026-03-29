8c1fc532844c5f098fff59d14ddf1c681cb734d2
sam blenny <68084116+samblenny@users.noreply.github.com>
2026-03-29 02:00:45 +0000

    add tests for utf-8 decoder

--- components/utf8/test/test_utf8.c
+++ components/utf8/test/test_utf8.c
@@ -4,8 +4,26 @@
 #include "unity.h"
 #include "utf8.h"
 
-TEST_CASE("check utf-8 decoder", "[utf8]")
+TEST_CASE("decode 1-byte codepoint: U+41", "[utf8]")
 {
-    // TODO: do stuff
-    TEST_ASSERT_EQUAL(1, 1);
+    uint32_t n = utf8_decode_codepoint("A", 0, 1);
+    TEST_ASSERT_EQUAL(0x00000041, n);
+}
+
+TEST_CASE("decode 2-byte codepoint: U+3c9", "[utf8]")
+{
+    uint32_t n = utf8_decode_codepoint("ω", 0, 2);
+    TEST_ASSERT_EQUAL(0x000003c9, n);
+}
+
+TEST_CASE("decode 3-byte codepoint: U+4f60", "[utf8]")
+{
+    uint32_t n = utf8_decode_codepoint("你", 0, 3);
+    TEST_ASSERT_EQUAL(0x00004f60, n);
+}
+
+TEST_CASE("decode 4-byte codepoint: U+1f600", "[utf8]")
+{
+    uint32_t n = utf8_decode_codepoint("😀", 0, 4);
+    TEST_ASSERT_EQUAL(0x0001f600, n);
 }
